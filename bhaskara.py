76767d579dfcd0ea85ddf06af2b7804c01ceb6e4
Pedro Queiroz <89913468+Keirz@users.noreply.github.com>
2022-02-24 16:01:35 -0300

    Create bhaskara.py

---
+++ bhaskara.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+A,B,C = list(map(float,input().split()))
+d = B * B - 4 * A * C
+x = pow(d, .5)
+if (d < 0 or A == 0):
+    print("Impossivel calcular")
+else:
+    f = (-B + x) / (2 * A)
+    g = (-B - x) / (2 * A)
+    print("R1 = %.5lf"%f)
+    print("R2 = %.5lf"%g)
