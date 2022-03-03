3619357a9efd6d0833c7c60f8e75b3fde6a5b067
Pedro Queiroz <89913468+Keirz@users.noreply.github.com>
2022-03-02 21:05:12 -0300

    Create perigosos graozinhos.py

---
+++ perigosos graozinhos.py
@@ -0,0 +1,13 @@
+x  = int(input())
+  for i in range(x):
+    a = (int(input()
+    y = 1
+    z = 0
+    for yIndex in range(a):
+    z+=y
+    y*=2
+    z = (z)12000
+ 
+    if z<1:
+    z=0      
+print(f'{str(z).split(".")[0]}kg')
